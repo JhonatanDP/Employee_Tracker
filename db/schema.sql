3dabbe80aecae277b92105e8318b4706181a8e98
Jhonatan <jonathandiazp@gmail.com>
2022-09-25 16:06:45 -0400

    DB shema

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,26 @@ DROP TABLE IF EXISTS deparment;
 
 CREATE TABLE deparment (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
+  name VARCHAR(30) NO NULL
 );
 
 CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
-  salary DECIMAL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
   department_id INTEGER,
+  INDEX dep_ind (deparment_id),
   CONSTRAINT fk_department FOREIGN KEY (deparment_id) REFERENCES deparment(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id INTEGER,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL;
   manager_id INTEGER,
-  FOREIGN KEY manager_id REFERENCES employee(id);
-  CONSTRAINT uc_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  INDEX manager_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL;
 );
